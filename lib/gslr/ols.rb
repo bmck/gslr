04be929f4fba81cffdc0398101c536ff536ac05e
Bill McKinnon <bill.mckinnon@bmck.org>
2024-07-18 16:11:10 -0500

    Update ols.rb

--- lib/gslr/ols.rb
+++ lib/gslr/ols.rb
@@ -11,9 +11,9 @@ module GSLR
       y = df2[dep_var].to_a
       x = indep_vars.map{|c| df2[c].to_a}.transpose
 
-      Rails.logger.info { "#{__FILE__}:#{__LINE__} intercept = #{intercept}" }
+      # Rails.logger.info { "#{__FILE__}:#{__LINE__} intercept = #{intercept}" }
       model = GSLR::OLS.new(intercept: intercept)
-      Rails.logger.info { "#{__FILE__}:#{__LINE__} fit_intercept = #{fit_intercept}" }
+      # Rails.logger.info { "#{__FILE__}:#{__LINE__} fit_intercept = #{fit_intercept}" }
       model.fit(x, y, dep_var: dep_var, indep_vars: indep_vars)
       model
     end
@@ -40,7 +40,7 @@ module GSLR
       # read solution
       c_ptr = FFI.gsl_vector_ptr(c, 0)
       @coefficients = c_ptr[0, s2 * Fiddle::SIZEOF_DOUBLE].unpack("d*")
-      Rails.logger.info { "#{__FILE__}:#{__LINE__} fit_intercept = #{fit_intercept}" }
+      # Rails.logger.info { "#{__FILE__}:#{__LINE__} fit_intercept = #{fit_intercept}" }
       @intercept = @fit_intercept ? @coefficients.shift : 0.0
       @covariance = read_matrix(cov, s2)
       @chi2 = chisq[0, Fiddle::SIZEOF_DOUBLE].unpack1("d")
